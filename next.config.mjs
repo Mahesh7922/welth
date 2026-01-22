9878c4b29656481dc8bfa8624ccef4624147a85a
Mahesh Chauhan <maheshchauhan@Maheshs-MacBook-Air.local>
2026-01-22 12:09:41 +0530

    Preparing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  images: {
+    domains: ["randomuser.me"],
+  },
+  experimental:{
+    serverActions:{
+      bodySizeLimit: "5mb",
+    },
+  },
+}; 
 
 export default nextConfig;
